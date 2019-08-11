5e49b2bdb286ffe3f283f271e3250e2b038d3cfb
Francisco <franciscobarros754@gmail.com>
2019-08-11 10:40:36 -0300

    removido espaços

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Observable } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  private items: Observable<any[]>;
+export class AppComponent implements OnInit {
+  public items: Observable<any[]>;
 
   constructor(private dbFirestore: AngularFirestore) {}
 
