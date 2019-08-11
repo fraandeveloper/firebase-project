d7c464990b5f7bd0d99e9640cf6a160f5278371e
Francisco <franciscobarros754@gmail.com>
2019-08-11 10:38:23 -0300

    adicionado dependencia do angularfire2 e fazendo consulta com collection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-fire';
+export class AppComponent implements OnInit{
+  private items: Observable<any[]>;
+
+  constructor(private dbFirestore: AngularFirestore) {}
+
+  ngOnInit(): void {
+    this.items = this.dbFirestore.collection('items').valueChanges();
+  }
 }
